3eec2e876c6356f2fb8285da571c7fd80d632d72
flaviopifiator <flaviomariano1993@hotmail.com>
2016-10-19 18:27:44 -0300

    refactor name project

--- project/settings.py
+++ project/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'banda',
+    'evento',
+    'integrantes',
 ]
 
 MIDDLEWARE = [
@@ -49,7 +52,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'bandas.urls'
+ROOT_URLCONF = 'project.urls'
 
 TEMPLATES = [
     {
@@ -67,7 +70,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'bandas.wsgi.application'
+WSGI_APPLICATION = 'project.wsgi.application'
 
 
 # Database
