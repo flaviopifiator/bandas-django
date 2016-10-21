3de061da6f535893f172d73770e195b84c20711a
flaviopifiator <flaviomariano1993@hotmail.com>
2016-10-21 14:02:48 -0300

    model persona abstract

--- core/models.py
+++ core/models.py
@@ -14,3 +14,13 @@ class EventUser(models.Model):
         auto_now=True,
         editable=False
     )
+
+class Persona(models.Model):
+    class Meta:
+        abstract = True
+
+    nombre = models.CharField(max_length=20)
+    apellido = models.CharField(max_length=20)
+    sexo = models.CharField(max_length=9)
+    nacimiento = models.DateField()
+    edad = models.IntegerField()
