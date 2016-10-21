bb2e05a56747e056fd8326f1ad0d29d98a026c24
flaviopifiator <flaviomariano1993@hotmail.com>
2016-10-21 14:04:19 -0300

    model integrantes e instrumentos

--- integrantes/models.py
+++ integrantes/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from core.models import Persona
+from banda.models import Banda
 
-# Create your models here.
+class Instrumento(models.Model):
+    nombre = models.CharField(max_length=20)
+    marca = models.CharField(max_length=30)
+    descripcion = models.TextField(max_length=200)
+
+
+class Integrante(Persona, models.Model):
+    banda = models.ManyToManyField(Banda)
+    instrumento = models.ManyToManyField(Instrumento)
